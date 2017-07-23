03966e34d6a7ff773c7f38180a5f69541020e894
Justin Anderson <jus0610tin@gmail.com>
2017-07-22 20:46:50 -0400

    Added macOS, tvOS, and watchOS targets

---
+++ Cacher-Mac/NSImage+Utility.swift
@@ -0,0 +1,34 @@
+//
+//  NSImage+Utility.swift
+//  Cacher
+//
+//  Created by Justin Anderson on 7/22/17.
+//  Copyright © 2017 Mountain Buffalo Limited. All rights reserved.
+//
+
+import Cocoa
+
+public typealias UIImage = NSImage
+
+extension NSBitmapImageRep {
+    var png: Data? {
+        return representation(using: .PNG, properties: [:])
+        
+    }
+    func jpeg(compressionFactor: CGFloat) -> Data? {
+        return representation(using: .JPEG, properties: [NSImageCompressionFactor: compressionFactor])
+    }
+}
+extension Data {
+    var bitmap: NSBitmapImageRep? {
+        return NSBitmapImageRep(data: self)
+    }
+}
+
+public func NSImagePNGRepresentation(_ image: NSImage) -> Data? {
+    return image.tiffRepresentation?.bitmap?.png
+}
+
+public func NSImageJPEGRepresentation(_ image: NSImage, _ compressionFactor: CGFloat) -> Data? {
+    return image.tiffRepresentation?.bitmap?.jpeg(compressionFactor: 1.0)
+}
